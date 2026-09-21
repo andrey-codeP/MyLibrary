126a97594dfe3ca8f794b057ddb374dc470487b5
Andrey_2011 <busik.popov@gmail.com>
2026-09-21 22:49:35 +0300

    i create mini authentication

--- security/login.py
+++ security/login.py
@@ -8,3 +8,61 @@ from typing import Annotated
 from datetime import datetime, timedelta, timezone
 
 
+from config import settings
+
+SECRET_KEY = settings.JWT_SECRET_TOKEN
+
+oauth2_schemas = OAuth2PasswordBearer(tokenUrl="/token")
+
+
+def create_access_token(user_id: int) -> str:
+
+    payload = {"sub": str(user_id), "type": "access"}
+
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    payload.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        payload, SECRET_KEY, algorithm=settings.ACCESS_TOKEN_ALGORITHM
+    )
+    return encoded_jwt
+
+
+def get_current_user(token: Annotated[str, Depends(oauth2_schemas)]):
+    try:
+        payload = jwt.decode(
+            token, SECRET_KEY, algorithm=settings.ACCESS_TOKEN_ALGORITHM
+        )
+
+        if payload.get("type") != "access":
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="invalid token type",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="the token does not contain an ID",
+            )
+
+        return int(user_id)
+
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="the token has expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    except jwt.InvalidTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="the token is invalid",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
