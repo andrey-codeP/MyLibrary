9007f6b00786af7923b3bab9bfa5ba8e643fcd5a
Andrey_2011 <busik.popov@gmail.com>
2026-09-22 19:13:02 +0300

    fixed a bag with unique password

--- database/models/user_table.py
+++ database/models/user_table.py
@@ -5,12 +5,15 @@ from datetime import datetime, timezone
 from database.models.base import UserBase
 
 
-class User(UserBase):
+class UserTable(UserBase):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+
     username: Mapped[str] = mapped_column(String(20), unique=True, index=True, nullable=False)
-    hashed_password: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+
     is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
 
     created_at: Mapped[datetime] = mapped_column(
