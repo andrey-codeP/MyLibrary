a33b5f20c3f4083df1c219a2ca8cb6c6fa67b667
Andrey_2011 <busik.popov@gmail.com>
2026-09-23 23:32:23 +0300

    refactor(schemas): rename User-> UserInDb

--- schemas/user.py
+++ schemas/user.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel, Field, ConfigDict
 
 
-class User(BaseModel):
+class UserInDb(BaseModel):
     username: str = Field(..., min_length=3, max_length=20)
     password: str = Field(..., min_length=6, max_length=20, description="простой пароль от 6 символов до 20")
 
     model_config = ConfigDict(from_attributes=True)
+
+
